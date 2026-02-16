da5afebfc116131dcfcebabab3eb6ff1373f7fa7
heyeuu <2829004293@qq.com>
2026-02-16 15:40:17 +0800

    ci:add yaml-cpp to scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install basic build tools
 sudo apt-get update
-sudo apt-get install -y cmake build-essential gcc-14 g++-14 yq wget gnupg libeigen3-dev libopencv-dev
+sudo apt-get install -y cmake build-essential gcc-14 g++-14 yq wget gnupg libeigen3-dev libopencv-dev libyaml-cpp-dev 
 
 # Install OpenVINO
 wget -qO - https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo gpg --dearmor --output /usr/share/keyrings/intel-openvino-archive-keyring.gpg
